c2a265e1952c01c1d9e5e0d6d0ed586c33eeed11
ludsonfelipe <felipe_ludson@hotmail.com>
2023-08-22 19:13:27 -0300

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "playground-s-11-e1a8ea11"
+  project = "playground-s-11-adbfaed1"
   region = "us-central1"
 }
 
@@ -9,7 +9,13 @@ resource "google_sql_database_instance" "instance" {
     region           = "us-central1"
     settings {
         tier = "db-f1-micro"
+
         disk_size = "10"
+        database_flags {
+          name = "cloudsql.logical_decoding"
+          value = "on"
+        }
+
         ip_configuration {
 
             // Datastream IPs will vary by region.
@@ -52,6 +58,7 @@ resource "google_sql_user" "user" {
     name = "user"
     instance = google_sql_database_instance.instance.name
     password = random_password.pwd.result
+
 }
 
 output "key" {
@@ -70,10 +77,11 @@ resource "google_datastream_connection_profile" "default" {
         password = google_sql_user.user.password
         database = google_sql_database.db.name
     }
+
 }
 
 resource "google_storage_bucket" "auto-expire" {
-  name          = "my_bucket3123123123"
+  name          = "my_bucket312321312314323"
   location      = "us-central1"
 
 }
@@ -84,7 +92,48 @@ resource "google_datastream_connection_profile" "bucketz" {
     connection_profile_id = "my-profile-b"
 
     gcs_profile {
-        bucket    = "my_bucket3123123123"
+        bucket    = "my_bucket312321312314323"
         root_path = "/path"
     }
 }
+
+resource "google_datastream_stream" "default"  {
+    display_name = "Postgres to Cloud Storage"
+    location     = "us-central1"
+    stream_id    = "my-stream"
+    desired_state = "RUNNING"
+
+    source_config {
+        source_connection_profile = google_datastream_connection_profile.default.id
+        postgresql_source_config {
+            max_concurrent_backfill_tasks = 12
+            publication      = "publ"
+            replication_slot = "repl"
+            include_objects {
+                postgresql_schemas {
+                    schema = "public"
+                    postgresql_tables {
+                        table = "usuarios"
+
+                    }
+                }
+            }
+        }
+        
+    }
+    backfill_all {  
+    }
+
+    destination_config {
+        destination_connection_profile = google_datastream_connection_profile.bucketz.id
+        gcs_destination_config  {
+            file_rotation_mb = 200
+            file_rotation_interval = "60s"
+            json_file_format {
+                schema_file_format = "NO_SCHEMA_FILE"
+                compression = "GZIP"
+            }
+            }
+        }
+    }
+    
