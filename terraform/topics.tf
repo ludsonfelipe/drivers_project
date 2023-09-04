a79fcedd47db92afd3e58a3d439aa6c92da02bf4
ludsonfelipe <felipe_ludson@hotmail.com>
2023-09-04 13:36:21 -0300

    topics

--- terraform/topics.tf
+++ terraform/topics.tf
@@ -6,7 +6,7 @@ resource "google_pubsub_topic" "users" {
   name = var.topic_users
 }
 
-resource "google_pubsub_topic" "travel" {
+resource "google_pubsub_topic" "travels" {
   name = var.topic_travel
 }
 
@@ -29,8 +29,59 @@ resource "google_pubsub_subscription" "gcs_drivers" {
   ]
 }
 
+resource "google_pubsub_subscription" "gcs_travels" {
+  name  = "travel-subscription-gcs"
+  topic = google_pubsub_topic.travels.name
+
+  cloud_storage_config {
+    bucket = google_storage_bucket.travels.name
+
+    filename_prefix = "pre-"
+    filename_suffix = "-${random_id.random_number.hex}"
+
+    max_bytes = 1000
+    max_duration = "60s"
+  }
+  depends_on = [ 
+    google_storage_bucket.travels,
+    google_storage_bucket_iam_member.admin,
+  ]
+}
+
+resource "google_pubsub_subscription" "gcs_users" {
+  name  = "users-subscription-gcs"
+  topic = google_pubsub_topic.users.name
+
+  cloud_storage_config {
+    bucket = google_storage_bucket.users.name
+
+    filename_prefix = "pre-"
+    filename_suffix = "-${random_id.random_number.hex}"
+
+    max_bytes = 1000
+    max_duration = "60s"
+  }
+  depends_on = [ 
+    google_storage_bucket.users,
+    google_storage_bucket_iam_member.admin,
+  ]
+}
+
+
 resource "google_storage_bucket_iam_member" "admin" {
   bucket = google_storage_bucket.drivers.name
+  role   = "roles/storage.admin"
+  member = "serviceAccount:service-218122086508@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "admin" {
+  bucket = google_storage_bucket.users.name
+  role   = "roles/storage.admin"
+  member = "serviceAccount:service-218122086508@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+resource "google_storage_bucket_iam_member" "admin" {
+  bucket = google_storage_bucket.travels.name
   role   = "roles/storage.admin"
   member = "serviceAccount:service-218122086508@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
